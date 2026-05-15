92001572167bd9a121fe6cd08e9b7ba1594339ab
Karim Baynashоv <karimbaynashov@MacBook-Pro-Karim.local>
2026-05-15 13:33:18 +0500

    updated

--- main.py
+++ main.py
@@ -209,5 +209,5 @@ async def main():
     print("🤖 Отандық Анон Чат Боты іске қосылды!")
     await dp.start_polling(bot)
 
-if name == "__main__":
+if __name__ == "__main__":
     asyncio.run(main())
